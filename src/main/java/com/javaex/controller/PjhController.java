9cdd57d9f77625d6b6fbb9e84f108ac7e140b0b7
pjh990909 <pjh990909@naver.com>
2024-05-20 17:33:06 +0900

    박종희

--- src/main/java/com/javaex/controller/PjhController.java
+++ src/main/java/com/javaex/controller/PjhController.java
@@ -2,14 +2,64 @@ package com.javaex.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.PjhService;
 import com.javaex.util.JsonResult;
+import com.javaex.util.JwtUtil;
+import com.javaex.vo.PjhVo;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class PjhController {
-	
-	@Autowired PjhService pjhService;
-	
+
+	@Autowired
+	PjhService pjhService;
+
+	// 회원가입
+	@PostMapping("/api/walking/joinpage")
+	public JsonResult joinpage(@RequestBody PjhVo users_listVo) {
+		System.out.println("PjhController.joinpage()");
+		int count = pjhService.exejoinpage(users_listVo);
+		System.out.println(count);
+
+		return JsonResult.success("");
+	}
+
+	// id 중복체크
+	@GetMapping("/api/walking/joinpageidcheck/{users_id}")
+	public JsonResult joinpageidcheck(@PathVariable("users_id") String usersid) {
+		System.out.println("PjhController.joinpageidcheck()");
+		System.out.println(usersid);
+
+		PjhVo userslistVo = new PjhVo();
+
+		userslistVo.setUsers_id(usersid);
+
+		int count = pjhService.exejoinpageidcheck(usersid);
+
+		return JsonResult.success(count);
+	}
+
+	// 로그인
+	@PostMapping("/api/walking/loginpage")
+	public JsonResult login(@RequestBody PjhVo users_listVo, HttpServletResponse response) {
+		System.out.println("UserController.login()");
+
+		PjhVo authUser = pjhService.exeLogin(users_listVo);
+
+		if (authUser != null) {
+			// 토큰발급 해더에 실어 보낸다
+			JwtUtil.createTokenAndSetHeader(response, "" + authUser.getUsers_no());
+			return JsonResult.success(authUser);
+		} else {
+
+			return JsonResult.fail("로그인실패");
+		}
+
+	}
 }
--- src/main/java/com/javaex/service/PjhService.java
+++ src/main/java/com/javaex/service/PjhService.java
@@ -1,8 +1,5 @@
 package com.javaex.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +9,34 @@ import com.javaex.vo.PjhVo;
 @Service
 public class PjhService {
 
-	//제현이꺼
-	@Autowired PjhDao kd;
-	
-	
+	@Autowired
+	PjhDao pjhDao;
+
+	// 회원가입
+	public int exejoinpage(PjhVo users_listVo) {
+		System.out.println("PjhService.exejoinpage()");
+		int count = pjhDao.join(users_listVo);
+		System.out.println(count);
+
+		return count;
+	}
+
+	// 중복체크
+	public int exejoinpageidcheck(String id) {
+		System.out.println("PjhService.exejoinpageidcheck");
+
+		int count = pjhDao.idCheck(id);
+
+		return count;
+	}
+
+	// 로그인
+	public PjhVo exeLogin(PjhVo users_listVo) {
+		System.out.println("UserService.exeLogin()");
+
+		PjhVo authUser = pjhDao.userSelscetByIdPw(users_listVo);
+
+		return authUser;
+	}
+
 }
