9e75fc54a495efcec8e0c5aa1a5a98869ededaad
seed-yoo <lukdown10@naver.com>
2024-06-11 20:19:41 +0900

    yys

--- src/main/java/com/javaex/config/WebMvcConfig.java
+++ src/main/java/com/javaex/config/WebMvcConfig.java
@@ -9,20 +9,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
 	/*
-	// flutter 용
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/api/**") // 경로
-				.allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*");
-	}
-	*/
-	
+	 * // flutter 용
+	 * 
+	 * @Override public void addCorsMappings(CorsRegistry registry) {
+	 * registry.addMapping("/api/**") // 경로 .allowedMethods("GET", "POST", "PUT",
+	 * "DELETE").allowedOrigins("*"); }
+	 */
+
 	// vue(html포함) 용
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**") // 경로
 				.allowedMethods("GET", "POST", "PUT", "DELETE")
-				.allowedOrigins("http://localhost:8080", "http://localhost:8081", "https://www.walkingstep.site")
+				.allowedOrigins("http://localhost:8080", "http://localhost:8081", "https://www.walkingstep.site",
+						"http://www.walkingstep.site", "https://walkingstep.site", "http://walkingstep.site",
+						"http://43.201.96.200", "https://43.201.96.200")
 				.allowedHeaders("*") // 모든 요청해더
 				.exposedHeaders("Authorization")// 노출시킬 헤더
 				.allowCredentials(true); // 쿠키허용
@@ -30,34 +31,22 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		
+
 		String saveDir;
-		
-		
+
 		String osName = System.getProperty("os.name").toLowerCase();
 		if (osName.contains("linux")) {
-        	System.out.println("리눅스");
-        	// 파일저장디렉토리
-        	saveDir = "/app/upload/"; // Linux 경로. username을 실제 사용자 이름으로 변경하세요.
-        
+			System.out.println("리눅스");
+			// 파일저장디렉토리
+			saveDir = "/app/upload/"; // Linux 경로. username을 실제 사용자 이름으로 변경하세요.
+
 		} else {
-        	System.out.println("윈도우");
-        	// 파일저장디렉토리
-        	saveDir = "D:\\javaStudy\\upload\\";
-        }
-		
-		
-		
-		registry.addResourceHandler("/upload/**")
-				.addResourceLocations("file:"+saveDir);
+			System.out.println("윈도우");
+			// 파일저장디렉토리
+			saveDir = "D:\\javaStudy\\upload\\";
+		}
+
+		registry.addResourceHandler("/upload/**").addResourceLocations("file:" + saveDir);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
